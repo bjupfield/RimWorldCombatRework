ec6755b9585136a508ee7793c43d40d5dcfd3a9c
Bennett Upfield <bjupfield@gmail.com>
2023-12-24 15:45:48 -0600

    Finally got the ability to access the string name of a def

--- Source/CombatRework/CombatRework.cs
+++ Source/CombatRework/CombatRework.cs
@@ -34,8 +34,47 @@ public static class Dialog_ArmorUtility_ApplyArmor_Patch
         var lineList = new List<CodeInstruction>(lines);
         for (int i = 0; i < lineList.Count(); i++)
         {
-            Verse.Log.Warning(lineList[i].ToString());
+            if (lineList[i].opcode == OpCodes.Ldarg_S) Verse.Log.Warning(lineList[i].ToString());
         }
+        //int finder = 0;
+        //int foundOperand = 0;
+        //while(finder < lineList.Count)
+        //{
+        //    finder++;
+        //    foundOperand++;
+        //    Verse.Log.Warning((lineList[foundOperand].operand != null ? lineList[foundOperand].operand.ToString() : "NOT APPLICABLE"));
+        //    if ((lineList[foundOperand].operand != null ? lineList[foundOperand].operand.ToString() : "NOT APPLICABLE") == "damageDef")
+        //    {
+        //        finder = lineList.Count;
+        //    }
+        //}
+
+        List<CodeInstruction> myInstructs = new List<CodeInstruction>();
+
+        //load ldarg.4
+        myInstructs.Add(new CodeInstruction(OpCodes.Ldarg_S, 4));
+
+        myInstructs.Add(new CodeInstruction(OpCodes.Ldind_Ref, null));
+
+        Type thingDefType = typeof(DamageDef);
+        //ldfld class verse.thingdef vese.thing::defName
+        myInstructs.Add(new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(thingDefType, "defName")));
+        Type[] myParams = { typeof(string) };
+        //calls Verse.Log.Warning(DamaageDef.defName);
+        myInstructs.Add(CodeInstruction.Call(typeof(Verse.Log), "Warning", myParams));
+
+        for(int i = 0; i < myInstructs.Count; i++)
+        {
+            Verse.Log.Warning(myInstructs[i].ToString());
+        }
+
+
+        lineList.InsertRange(0, myInstructs);
+
+        //for (int i = 0; i < lineList.Count; i++)
+        //{
+        //    Verse.Log.Warning(lineList[i].ToString());
+        //}
         return lineList;
     }
 }
